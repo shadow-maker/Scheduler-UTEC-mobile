87d611dd7ce8536220a1dede49f9a59ed5eefc27
Carlos Andres <carlos.andres.montoro@gmail.com>
2021-08-02 13:55:00 -0500

    Add button listenter

--- SchedulerUtec/app/src/main/java/com/grupoDBP/schedulerutec/ScheduleExploreActivity.java
+++ SchedulerUtec/app/src/main/java/com/grupoDBP/schedulerutec/ScheduleExploreActivity.java
@@ -1,10 +1,18 @@
 package com.grupoDBP.schedulerutec;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.*;
 
@@ -31,5 +39,10 @@ public class ScheduleExploreActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
+    }
+
+    // ------------ BUTTON CLICK LISTENERS -----------------------
+    public void onClickScheduleButty(View view){
+        // TODO
     }
 }
