1ffa2088d327af091cc05d6b1540e1cbbf433235
Rodrigo Salazar <rodrigo.salazar@utec.edu.pe>
2021-08-02 09:07:18 -0500

    ScheduleEdit Update Title implemented

--- SchedulerUtec/app/src/main/java/com/grupoDBP/schedulerutec/ScheduleEditActivity.java
+++ SchedulerUtec/app/src/main/java/com/grupoDBP/schedulerutec/ScheduleEditActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -48,6 +50,23 @@ public class ScheduleEditActivity extends AppCompatActivity {
             e.printStackTrace(); return;
         }
 
+        // --------------------------- NAME AND CREATOR -------------------
+        try {
+            // Get views
+            TextView studentTextView = findViewById(R.id.schedule_view_student);
+            TextView titleTextView = findViewById(R.id.schedule_view_name);
+            // Get student data
+            String schedule_name= jsonData.getString("horario_titulo");
+            String schedule_student_first_name = jsonData.getString("horario_alumno_nombre");
+            String schedule_student_last_name = jsonData.getString("horario_alumno_apellido");
+            // Set text
+            studentTextView.setText(getString(R.string.schedule_view_student_txt, schedule_student_first_name, schedule_student_last_name));
+            titleTextView.setText(getString(R.string.schedule_view_name_txt, schedule_name));
+        } catch (JSONException e) {
+            // Error Handeling
+            Log.e(this.getClass().getName(), "Invalid API Response (No student/title data found)");
+            e.printStackTrace();
+        }
 
         // ---------------------------- LOAD TABLE ---------------------------
         TableLayout tb = findViewById(R.id.schedule_view_table_layout);
@@ -65,20 +84,34 @@ public class ScheduleEditActivity extends AppCompatActivity {
     }
 
     public void onClickUpdateTitleSubmit(View view){
-        /*
         // Retrieve data
         EditText titleInput = findViewById(R.id.rename_edit);
         String schedule_title = titleInput.getText().toString();
 
+        // Get view to be updated
+        TextView titleTextView = findViewById(R.id.schedule_view_name);
+
         // Submit Request through API
-        JSONObject newScheduleJSON;
+        boolean success;
         try {
-            newScheduleJSON = RequestHandeler.createScheduleRequest(schedule_title);
+            success = RequestHandeler.updateScheduleTitleRequest(schedule_title);
         } catch (JSONException e) {
+            Toast.makeText(this, getString(R.string.generic_connection_error), Toast.LENGTH_SHORT).show();
             e.printStackTrace();
-            Log.w(this.getClass().getName(),"Invalid API response when creating new schedule");
+            Log.w(this.getClass().getName(),"Invalid API response when updating schedule'stitle");
             return;
         }
-        */
+        if (success) {
+            // Success
+            titleTextView.setText(getString(R.string.schedule_view_name_txt, schedule_title));
+            Toast.makeText(this, getString(R.string.schedule_edit_title_update_success_txt), Toast.LENGTH_SHORT).show();
+            Log.v(this.getClass().getName(),"Schedule's title updated succesfully");
+        }
+        else {
+            // Success
+            Toast.makeText(this, getString(R.string.generic_invalid_response), Toast.LENGTH_SHORT).show();
+            Log.w(this.getClass().getName(),"Could not update schedule's title");
+        }
+
     }
 }
